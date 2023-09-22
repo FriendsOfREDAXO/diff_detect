2c4f7233a0728072c4147161140cd59cc6d3c2da
Jan Kristinus <jan.kristinus@yakamara.de>
2023-09-22 22:33:22 +0200

    CS

--- tests/dummy_test.php
+++ tests/dummy_test.php
@@ -2,10 +2,13 @@
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ */
 class rex_diff_detect_dummy_test extends TestCase
 {
-  public function testSettings()
-  {
-    $this->assertTrue(true);
-  }
+    public function testSettings()
+    {
+        static::assertTrue(true);
+    }
 }
